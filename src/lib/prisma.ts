c322d30cd853a012425bf130065188d6659d7b19
Matias Galvan Guerrero <matias.galvan@tecsup.edu.pe>
2026-06-06 10:44:33 -0500

    Fix prisma vercel deploy

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,14 +2,17 @@ import { PrismaPg } from '@prisma/adapter-pg'
 
 const connectionString = "postgresql://postgres.cgfpiauedwiysxisxeyj:RQe3&4QsM.j5d.W@aws-1-us-east-1.pooler.supabase.com:5432/postgres"
 
-const adapter = new PrismaPg({ connectionString })
-
-const { PrismaClient } = require('@prisma/client')
-
 declare global {
   var prisma: any
 }
 
-export const prisma = global.prisma || new PrismaClient({ adapter })
+function createPrismaClient() {
+  const adapter = new PrismaPg({ connectionString })
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { PrismaClient } = require('@prisma/client/default')
+  return new PrismaClient({ adapter })
+}
+
+export const prisma = global.prisma || createPrismaClient()
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma
