a9863e2eaa19f487b1fb9abab77df6e2ae495b68
Matias Galvan Guerrero <matias.galvan@tecsup.edu.pe>
2026-06-06 10:38:49 -0500

    Fix prisma require

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,15 @@
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '@prisma/client'
 
 const connectionString = "postgresql://postgres.cgfpiauedwiysxisxeyj:RQe3&4QsM.j5d.W@aws-1-us-east-1.pooler.supabase.com:5432/postgres"
+
+const adapter = new PrismaPg({ connectionString })
+
+const { PrismaClient } = require('@prisma/client')
 
 declare global {
   var prisma: any
 }
 
-const adapter = new PrismaPg({ connectionString })
-
-export const prisma = global.prisma || new (PrismaClient as any)({ adapter })
+export const prisma = global.prisma || new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma
