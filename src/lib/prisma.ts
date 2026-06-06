025479439c4413115c7eb41124ca98c4e659ca4d
Matias Galvan Guerrero <matias.galvan@tecsup.edu.pe>
2026-06-06 10:34:23 -0500

    Fix prisma client v7

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,14 @@
-import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '@prisma/client'
 
 const connectionString = "postgresql://postgres.cgfpiauedwiysxisxeyj:RQe3&4QsM.j5d.W@aws-1-us-east-1.pooler.supabase.com:5432/postgres"
 
-const adapter = new PrismaPg({ connectionString })
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+declare global {
+  var prisma: any
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter } as any)
+const adapter = new PrismaPg({ connectionString })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export const prisma = global.prisma || new (PrismaClient as any)({ adapter })
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
