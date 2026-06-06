adc485501b195d44c6256869e97dcc54702158b0
Matias Galvan Guerrero <matias.galvan@tecsup.edu.pe>
2026-06-06 10:28:12 -0500

    Fix PrismaClient para Vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 
-const adapter = new PrismaPg({
-  connectionString: "postgresql://postgres.cgfpiauedwiysxisxeyj:RQe3&4QsM.j5d.W@aws-1-us-east-1.pooler.supabase.com:5432/postgres",
-})
+const connectionString = "postgresql://postgres.cgfpiauedwiysxisxeyj:RQe3&4QsM.j5d.W@aws-1-us-east-1.pooler.supabase.com:5432/postgres"
+
+const adapter = new PrismaPg({ connectionString })
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter } as any)
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
